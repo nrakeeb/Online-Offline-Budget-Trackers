2bba1f22555655c4601fedc4462b305f83c728ef
nrakeeb <nouha_rakeeb@hotmail.co.uk>
2021-12-10 02:55:36 +0000

    chore: added save record function
    Authors: NR

--- Develop/public/js/db.js
+++ Develop/public/js/db.js
@@ -14,3 +14,9 @@ if (navigator.onLine) {
 request.onerror = function (event) {
   console.log("Woops! " + event.target.errorCode);
 };
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
